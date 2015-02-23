b27b1b4e82e964e7bc5c8569f3125eaeb1e84fb3
mbohun <martin.bohun@gmail.com>
2015-02-23 13:24:30 +1100

    cleanup: pass the log file, and error regex in arguments, instead of hardcoding them in the script

--- test/test-errors-per-day/extract-errors-per-day.sh
+++ test/test-errors-per-day/extract-errors-per-day.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "usage: $0 [log file] [error regexp]"
+    exit 1;
+fi
+
+LOG_FILE=$1
+ERROR_REGEXP=$2
+
 temp=`basename $0`
 ERROR_TIMESTAMPS=`mktemp /tmp/${temp}.XXXXXX` || exit 1
-
-ERROR_REGEXP='proxy error'
 
 # TODO: add proper input args processing, tmp files, etc
 
 # for each log file if we have many, collect $ERROR_PATTER and accumulate the matches in some tmp file
-grep -i 'proxy error' ./biocache-service.log | grep '^201[5-9]' | sed -e 's/ \[org.*$//g' > $ERROR_TIMESTAMPS
+eval grep $ERROR_REGEXP $LOG_FILE | grep '^201[5-9]' | sed -e 's/ \[org.*$//g' > $ERROR_TIMESTAMPS
 
 # for each day extract the number of errors
 for day in `cat $ERROR_TIMESTAMPS | sed -e 's/ .*$//g' | sort | uniq`
