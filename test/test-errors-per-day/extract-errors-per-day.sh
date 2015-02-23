27a231142688fcce7b667d5e0061410524ad2254
mbohun <martin.bohun@gmail.com>
2015-02-23 12:45:49 +1100

    use proper tmp file for the error timestamp processing

--- test/test-errors-per-day/extract-errors-per-day.sh
+++ test/test-errors-per-day/extract-errors-per-day.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+temp=`basename $0`
+ERROR_TIMESTAMPS=`mktemp /tmp/${temp}.XXXXXX` || exit 1
+
 ERROR_REGEXP='proxy error'
 
 # TODO: add proper input args processing, tmp files, etc
 
 # for each log file if we have many, collect $ERROR_PATTER and accumulate the matches in some tmp file
-grep -i 'proxy error' ./biocache-service.log | grep '^201[5-9]' | sed -e 's/ \[org.*$//g' > proxy_error-all.dat
+grep -i 'proxy error' ./biocache-service.log | grep '^201[5-9]' | sed -e 's/ \[org.*$//g' > $ERROR_TIMESTAMPS
 
 # for each day extract the number of errors
-for day in `cat proxy_error-all.dat | sed -e 's/ .*$//g' | sort | uniq`
+for day in `cat $ERROR_TIMESTAMPS | sed -e 's/ .*$//g' | sort | uniq`
 do
-    counter=`grep ${day} proxy_error-all.dat | wc -l`
+    counter=`grep ${day} $ERROR_TIMESTAMPS | wc -l`
     echo "${day} ${counter}"
 done
