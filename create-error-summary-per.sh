2cc0d5da3f203424e8377c5813ca61abe5478a4d
mbohun <martin.bohun@gmail.com>
2015-02-27 14:43:04 +1100

    enhanced version of the create-error-summary.sh script that allow to specify if the summary data should be per day, per minute, per second, etc

---
+++ create-error-summary-per.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "usage: $0 [log file]"
+    exit 1;
+fi
+
+LOG_FILE=$1
+
+# first delete all lines NOT beginning with a timestamp (this strips all the exceptions stack traces, details we do not need)
+grep '^201[4-9]-[0-1][0-9]-[0-3][0-9].*\[[a-zA-Z0-9.]*\]' $LOG_FILE | sed -e 's/].*$/]/g' > tmp.log
+
+for err in `sed -e 's/^.*\[/[/' tmp.log | sort | uniq`; do
+    regexp='^201[4-9]-[0-1][0-9]-[0-3][0-9].*\\$err'
+    err_count=`eval grep $regexp tmp.log | wc -l`
+    echo "$err $err_count"
+done
